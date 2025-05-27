01c3b06cf0fe138fe60ec5e313f9795fb0a72cda
Grey-Liu <sugarfree5807@gmail.com>
2025-05-27 11:57:39 +0800

    add 99

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
 console("Hollo world!")
+
+let html = "<table border='1' cellspacing='0' cellpadding='5'>";
+for (let i = 1; i <= 9; i++) {
+    html += "<tr>";
+    for (let j = 1; j <= 9; j++) {
+        html += `<td>${i} × ${j} = ${i * j}</td>`;
+    }
+    html += "</tr>";
+}
+html += "</table>";
+document.write(html);
